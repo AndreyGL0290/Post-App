9b72efa763460d591ee33aee30755ef9827f1db0
AndreyGL0290 <andreygl2014@gmail.com>
2023-05-22 20:06:00 -0400

    Removed debug mode

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, redirect, request
 from DB.posts.pull_posts import pull_posts
 from DB.posts.push_post import push_post
 from DB.posts.delete_post import delete_post
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.get('/')
 def homePage():
@@ -28,4 +28,4 @@ def postPage():
     return render_template('newPost.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000)
