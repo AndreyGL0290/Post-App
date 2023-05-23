54a3c02eabc8ba668fc16a6d8e4dc86526ad23e0
AndreyGL0290 <andreygl2014@gmail.com>
2023-05-22 21:12:32 -0400

    Opening DB based on DB_PATH environmental variable

--- DB/connection.py
+++ DB/connection.py
@@ -1,8 +1,9 @@
+from os import getenv
 import sqlite3
 
 class SQLiteConnection():
-    def __init__(self) -> None:       
-        self.conn = sqlite3.connect('DB/posts.db')
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect(getenv('DB_PATH'))
         self.cur = sqlite3.Cursor(self.conn)
     
     def __enter__(self, *args, **kwargs):
